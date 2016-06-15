ab8793e411b706deee75115b0b871c4b707a3a75
Frederico.Pedregosa <z060911@6c4008b15846.hq.target.com>
2016-06-15 16:26:35 -0500

    Initial

--- src/app/buyproducts.component.ts
+++ src/app/buyproducts.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { ProductsComponent } from './+products';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'buyproducts-app',
   templateUrl: 'buyproducts.component.html',
-  styleUrls: ['buyproducts.component.css']
+  styleUrls: ['buyproducts.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/products', component: ProductsComponent}
+])
 export class BuyproductsAppComponent {
   title = 'buyproducts works!';
 }
